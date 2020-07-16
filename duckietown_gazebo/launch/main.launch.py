a287932e33610d1d542887fe5a7618673f0b50ac
Michael Equi <michaelrequi@gmail.com>
2020-07-16 15:07:16 -0700

    fixed env variable problem

--- duckietown_gazebo/launch/main.launch.py
+++ duckietown_gazebo/launch/main.launch.py
@@ -15,7 +15,10 @@ def generate_launch_description():
     duckietown_dir = get_package_share_directory('duckietown_gazebo')
 
     # export models path
-    os.environ['GAZEBO_MODEL_PATH'] = os.getenv('GAZEBO_MODEL_PATH') + ":" + duckietown_dir + "/models"
+    if os.getenv('GAZEBO_MODEL_PATH') is not None:
+        os.environ['GAZEBO_MODEL_PATH'] = os.getenv('GAZEBO_MODEL_PATH') + ":" + duckietown_dir + "/models"
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = duckietown_dir + "/models"
 
     world = LaunchConfiguration('world')
 
